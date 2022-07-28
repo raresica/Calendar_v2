283668adf0337fdd4016967ed5dd358e7c5fe96f
raresonescu <raresonescu@gmail.com>
2022-07-28 13:42:05 +0300

    get some data from database

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script defer src="script.js"></script>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="calendar-content">
+    <div class="calendar-header">
+        <button id="prev"><</button>
+        <h2><span id="months"> </span><span id="years"> </span></h2>
+        <button id="next">></button>
+    </div>
+
+    <div id="calendar-content" id="">
+
+    </div>
+</div>
+
+<div class="modal-container"
+     id="modal_container">
+    <div class="modal">
+        <h1>Date: </h1>
+        <p> <?php GetAccounts("first_name"); ?> </p>
+
+
+        <button id="close">
+    Inchide
+        </button>
+    </div>
+</div>
+</body>
+</html>
+
 <?php
-echo "<h1>This is served with mysql Server(dupa ani si ani)</h1>";
 
-$pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$query = $pdo->query('SHOW VARIABLES like "version"');
+function GetAccounts($first_name){
+    $pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $stmt = $pdo->prepare("SELECT * FROM user 
+         where user.first_name = :user_first_name");
+    $stmt->bindParam(':user_first_name',  $first_name);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
-$row = $query->fetch();
+    $row=$stmt->fetch();
+    $row["first_name"];
+    echo $row["first_name"];
+}
 
-echo 'MySQL version:' . $row['Value'];
 
-var_dump($row);
+?>
